2cfddfbd800fae45d77ae9f14633ca93f9c0f5a4
christylaguardia <christinelaguardia@gmail.com>
2017-04-23 16:35:35 -0700

    Peer programming with Christy Erdem and Andrew on Christy's birthday

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -1,6 +1,7 @@
 //  REVIEW: Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var articleView = {};
 
+
 articleView.populateFilters = function() {
   $('article').not('.template').each(function() {
     var authorName, category, optionTag;
@@ -17,16 +18,20 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-    if ($(this).val()) {
+    var selection = $(this).val();
+    if (selection) {
       /* TODO: If the select box changes to an option that has a value, we should:
           1. Hide all of the articles
           2. Fade in only the articles that match based on on the author
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
+      $('article').hide();
+      $('article[data-author="' + selection + '"]').fadeIn(800);
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
+        $('article').not('.template').show();
     }
     $('#category-filter').val('');
   });
@@ -36,6 +41,16 @@ articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function () {
+    var selection = $(this).val();
+    if (selection) {
+      $('article').hide();
+      $('article[data-category="' + selection + '"]').fadeIn(800);
+    } else {
+      $('article').not('.template').show();
+    }
+    $('#author-filter').val('');
+  })
 };
 
 articleView.handleMainNav = function () {
@@ -45,6 +60,15 @@ articleView.handleMainNav = function () {
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
+    $('.tab-content').hide();
+    var selection = $(this).attr('data-content');
+    if (selection === 'articles') {
+      $('#articles').show();
+      $('#about').hide();
+    } else if (selection === 'about') {
+      $('#articles').hide();
+      $('#about').show();
+    }
   });
   $('.main-nav .tab:first').click();
 };
@@ -60,6 +84,20 @@ articleView.setTeasers = function() {
 
     // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
   */
-};
+  $('.read-on').click(function() {
+    if($(this).text() === 'Read on') {
+      $(this).siblings('section').find('p').fadeIn(423);
+      $(this).text('Show Less');
+    } else if ($(this).text() === 'Show Less'){
+      $('.article-body *:nth-of-type(n+2)').hide();
+      $(this).text('Read on');
+    }
+  });
+}
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+articleView.handleMainNav();
+articleView.setTeasers();
